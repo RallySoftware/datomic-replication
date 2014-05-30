29038576f139d3cf77ae46b264fba2a6508c34e9
Chris Perkins <cperkins@rallydev.com>
2014-05-30 07:39:43 -0600

    WIP - lots of messy, redundant code as I figure stuff out

--- src/datomic_replication/core.clj
+++ src/datomic_replication/core.clj
@@ -1,34 +1,11 @@
 (ns datomic-replication.core
   (:require [clojure.core.async :as async :refer [go go-loop <! >!]]
-            [datomic.api :as datomic]
+            [datomic.api :as d]
             [enos.core :refer [dochan!]]))
 
 
 ;;; FIXME - need a marker per source+destination pair
 (def marker-name "/datomic/replication/last-tx")
-
-(defn transactions
-  "Returns an async channel of transactions, starting at `from-t`."
-  ([conn]
-     (transactions conn nil))
-  ([conn from-t]
-     (transactions conn from-t 1000))
-  ([conn from-t poll-interval]
-     (let [ch        (async/chan)
-           continue? (atom true)
-           stopper   #(reset! continue? false)]
-       (go-loop [from-t from-t]
-         (when @continue?
-           (let [txs (datomic/tx-range (datomic/log conn) from-t nil)]
-             (if (seq txs)
-               (do
-                 (doseq [tx txs]
-                   (>! ch tx))
-                 (recur (inc (:t (last txs)))))
-               (do
-                 (<! (async/timeout poll-interval))
-                 (recur from-t))))))
-       [ch stopper])))
 
 (defn- init-dest-database
   "This is the default implementation of the `:init` function that you
@@ -36,11 +13,13 @@
   destination database that connects each entity to its corresponding
   entity in the source database."
   [conn]
-  (datomic/transact conn
-                    [{:db/ident              :datomic-replication/source-eid
-                      :db/valueType          :db.type/long
-                      :db/unique             :db.unique/identity
-                      :db.install/_attribute :db.part/db}]))
+  @(d/transact conn
+               [{:db/id                 (d/tempid :db.part/db)
+                 :db/ident              :datomic-replication/source-eid
+                 :db/valueType          :db.type/long
+                 :db/unique             :db.unique/identity
+                 :db/cardinality        :db.cardinality/one
+                 :db.install/_attribute :db.part/db}]))
 
 (defn- e->id-default
   "This is the default mechanism for getting a database-independent
@@ -51,27 +30,178 @@
   [db eid]
   [:datomic-replication/source-eid eid])
 
+(def default-opts
+  {:init          init-dest-database
+   :e->id         e->id-default
+   :poll-interval 100})
+
+(defn transactions
+  "Returns an async channel of transactions.
+   Options include:
+    - from-t - the `t` to start at
+    - poll interval - how long to pause when there are no new transactions
+  "
+  ([conn]
+     (transactions conn nil))
+  ([conn opts]
+     (let [{:keys [from-t poll-interval]} (merge default-opts opts)
+           ch        (async/chan)
+           continue? (atom true)
+           stopper   #(reset! continue? false)]
+       (go-loop [from-t from-t]
+         (when @continue?
+           (let [txs (d/tx-range (d/log conn) from-t nil)]
+             (if (seq txs)
+               (do
+                 (doseq [tx txs]
+                   (>! ch tx))
+                 (recur (inc (:t (last txs)))))
+               (do
+                 (<! (async/timeout poll-interval))
+                 (recur from-t))))))
+       [ch stopper])))
+
+
+(defn partition-ident [db eid]
+  (:db/ident (d/entity db (d/part eid))))
+
+(defn skip-attr? [ident]
+  false)
+
+(defn tx->datoms
+  "Transforms a transaction into a sequence of datoms suitable for
+  transacting to the destination database."
+  [{:keys [t data] :as tx} source-conn e->id]
+  (let [source-db (d/as-of (d/db source-conn) t)]
+    (for [[e a v t added?] data
+          :let [[id-attr id-value] (e->id source-db e)
+                part (partition-ident source-db e)
+                attr-ident (:db/ident (d/entity source-db a))]
+          :when (not (skip-attr? attr-ident))]
+      (if added?
+        (hash-map
+         :db/id     (d/tempid part)
+         id-attr    id-value
+         attr-ident v)
+        [:db/retract
+         [id-attr id-value]
+         attr-ident
+         v]))))
+
+
+
+
+(def ^:dynamic *e->id*
+  "Function that returns the :ident of an attribute to use as the
+  database-independent identifier for the given entity."
+  (fn [db ent]
+    (if-let [ident (:db/ident ent)]
+      [:db/ident ident]
+      [:datomic-replication/source-eid (:db/id ent)])))
+
+(defn mapify-txs
+  "Transforms the :data sequence from a transaction into a sequence of maps.
+   [[e a v t added?] ...] => [{:eid e :added {a-ident v} :retracted {b-ident v}} ...]"
+  [db data]
+  (->> data
+       (reduce (fn [m [e a v t added?]]
+                 (let [attr-ident (:db/ident (d/entity db a))
+                       action     (if added? :added :retracted)]
+                   (assoc-in m [e action attr-ident] v)))
+               nil)
+       (map (fn [[eid m]] (assoc m :eid eid)))))
+
+(defn assoc-partition
+  [db m]
+  (let [part (partition-ident db (:eid m))]
+    (assoc m :part part)))
+
+(defn assoc-identity
+  "Given a map (as produced by mapify-txs), returns it with the ident
+  of the entity assoc'ed in, if it has one."
+  [db m]
+  (if (= :db.part/tx (:part m))
+    (assoc m :ident (d/tempid :db.part/tx))
+    (let [[id-attr id-value] (*e->id* db (d/entity db (:eid m)))]
+      (assoc m :ident [id-attr id-value]))))
+
+(defn ->datoms
+  [{:keys [eid ident added retracted]}]
+  (concat [(when-not (empty? added)
+             (assoc added :db/id ident))]
+          (for [[attr v] retracted]
+            [:db/retract
+             ident
+             attr
+             v])))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+;;; Transform the transaction by added some useful crap to it:
+;;;  - mapping from source-eid -> tempid
 
 (defn replicate-tx
   "Sends the transaction to the connection."
   [{:keys [t data] :as tx} source-conn dest-conn e->id]
-  (println "Got tx:" tx)
-  (let [source-db (datomic/as-of (datomic/db source-conn) t)
-        datoms    (for [[e a v t added?] data]
-                    [(if added? :db/add :db/retract)
-                     (e->id source-db e)
-                     (:db/ident (datomic/entity source-db a))
-                     v])
+  (prn "Got tx:" tx)
+  (let [source-db (d/as-of (d/db source-conn) t)
+        dest-db   (d/db dest-conn)
+        eids      (doall (distinct (for [[e] data] e)))
+        _  (prn "eids are " eids)
+
+        ;; Create a mapping from the distinct eids in the transaction
+        ;; to a tempid in the correct partition.
+        tempids   (into {}
+                        (for [eid eids]
+                          (let [part (partition-ident source-db eid)]
+                            [eid (d/tempid part)])))
+
+        ;; A mapping from eids to idents, for those entities that have idents.
+        idents    (into {}
+                        (for [eid eids
+                              :let [ent (d/entity source-db eid)
+                                    ident (:db/ident ent)]
+                              :when ident]
+                          [eid ident]))
+        
         datoms    (for [[e a v t added?] data
-                        :when added?
-                        :let [[id-attr id-value] (e->id source-db e)]]
-                    (hash-map
-                     :db/id (datomic/tempid :db.part/user)
-                     id-attr id-value
-                     (:db/ident (datomic/entity source-db a)) v))]
-    (prn "transacting:" datoms)
+                        :let [[id-attr id-value] (e->id source-db e)
+                              part (partition-ident source-db e)
+                              attr-ident (:db/ident (d/entity source-db a))]
+                        :when (not (skip-attr? attr-ident))]
+                    (if added?
+                      (if-let [ident (idents e)]
+                        (hash-map
+                         :db/id     ident
+                         attr-ident v)
+                        (hash-map
+                         :db/id     (tempids e)
+                         id-attr    id-value
+                         attr-ident v))
+                      [:db/retract
+                       [id-attr id-value]
+                       attr-ident
+                       v]))
+
+        ;; _         (->> tx
+        ;;                :data
+        ;;                (mapify-txs source-db)
+        ;;                (map (partial assoc-partition source-db))
+        ;;                (map (partial assoc-identity source-db))
+        ;;                (mapcat ->datoms)
+        ;;                (remove nil?))
+        ]
+    (prn "transacting:")
+    (clojure.pprint/pprint datoms)
     (try
-      @(datomic/transact dest-conn datoms)
+      @(d/transact dest-conn datoms)
       (catch Exception e
         (println "Error transacting!!!!")
         (.printStackTrace e)
@@ -82,19 +212,14 @@
   (start [this])
   (stop  [this]))
 
-(def default-opts
-  {:init          init-dest-database
-   :e->id         e->id-default
-   :poll-interval 1000})
-
 (defn replicator
   "Returns a replicator that copies transactions from source-conn to dest-conn."
   ([source-conn dest-conn]
      (replicator source-conn dest-conn nil))
   ([source-conn dest-conn opts]
-     (let [{:keys [init e->id poll-interval]} (merge default-opts opts)
+     (let [{:keys [init e->id poll-interval] :as opts} (merge default-opts opts)
            control-chan  (async/chan)
-           [txs stopper] (transactions source-conn poll-interval)
+           [txs stopper] (transactions source-conn opts)
            initialized?  (atom false)]
        (reify Replicator
          (start [this]
@@ -104,8 +229,20 @@
                  (when-not @initialized?
                    (reset! initialized? true)
                    (init dest-conn))
-                 (replicate-tx tx source-conn dest-conn e->id)
+                 (try
+                   (replicate-tx tx source-conn dest-conn e->id)
+                   (catch Exception e
+                     (.printStackTrace e)
+                     (throw e)))
                  (recur)))))
          (stop [this]
            (stopper)
            (async/put! control-chan :stop))))))
+
+
+
+
+;;; Experimental - new middleware-based version
+
+;; tx -> group by e? -> determine partitions -> get txInstant -> add tx metadata (source tx, ...)
+;; -> recognize attr installs and add crap -> catch non-existing attrs and retry
--- test/datomic_replication/core_test.clj
+++ test/datomic_replication/core_test.clj
@@ -10,27 +10,27 @@
     (try
       (datomic/create-database uri1)
       (datomic/create-database uri2)
-      (let [db1        (datomic/connect uri1)
-            db2        (datomic/connect uri2)
-            replicator (rep/replicator db1 db2)]
+      (let [c1        (datomic/connect uri1)
+            c2        (datomic/connect uri2)
+            replicator (rep/replicator c1 c2)]
       
         ;; Start the replication
         (rep/start replicator)
 
         (try
-          ;; Do some stuff to the source database:
-          (let [result @(datomic/transact db1 [{:db/id                 (datomic/tempid :db.part/db)
-                                                :db/ident              :user/name
-                                                :db/valueType          :db.type/string
-                                                :db/cardinality        :db.cardinality/one
-                                                :db.install/_attribute :db.part/db}])]
+          ;; Create an attribute in the source database
+          (let [result @(datomic/transact c1 [{:db/id                 (datomic/tempid :db.part/db)
+                                               :db/ident              :user/name
+                                               :db/valueType          :db.type/string
+                                               :db/cardinality        :db.cardinality/one
+                                               :db.install/_attribute :db.part/db}])]
             (is result))
 
           ;; Wait a bit for it to replicate
-          (Thread/sleep 3000)
+          (Thread/sleep 500)
 
           ;; And make sure that the attribute got replicated
-          (is (= 1 (count (datomic/datoms :avet :db/ident :user/name))))
+          (is (= 1 (count (seq (datomic/datoms (datomic/db c2) :avet :db/ident :user/name)))))
 
         
           (finally
@@ -39,4 +39,43 @@
       (finally
         (datomic/delete-database uri1)
         (datomic/delete-database uri2)))))
+
+
+
+;; (deftest test-replication
+;;   (let [uri1 "datomic:free://localhost:4334/source"
+;;         uri2 "datomic:free://localhost:4334/dest"]
+;;     (testing "Schema already set up in destination"
+;;       (try
+;;         (datomic/create-database uri1)
+;;         (datomic/create-database uri2)
+;;         (let [c1         (datomic/connect uri1)
+;;               c2         (datomic/connect uri2)
+;;               replicator (rep/replicator c1 c2)]
+      
+;;           (try
+;;             ;; Create schema in both databases
+;;             (doseq [c [c1 c2]]
+;;               @(datomic/transact c [{:db/id                 (datomic/tempid :db.part/db)
+;;                                      :db/ident              :user/name
+;;                                      :db/valueType          :db.type/string
+;;                                      :db/cardinality        :db.cardinality/one
+;;                                      :db.install/_attribute :db.part/db}]))
+
+;;             ;; Start the replication
+;;             (rep/start replicator)
+
+;;             ;; Wait a bit for it to replicate
+;;             (Thread/sleep 3000)
+
+;;             ;; And make sure that the attribute got replicated
+;;             (is (= 1 (count (datomic/datoms :avet :db/ident :user/name))))
+
+        
+;;             (finally
+;;               (rep/stop replicator))))
+
+;;         (finally
+;;           (datomic/delete-database uri1)
+;;           (datomic/delete-database uri2))))))
 
