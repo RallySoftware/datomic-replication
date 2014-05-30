faaecae774470fa509a74ed773c241e49a37c216
IT Loaner <loaner@bld-loaner-10.local>
2014-05-30 13:16:24 -0600

    Works for the the one simple test-case that I have so far. Progress.

--- src/datomic_replication/core.clj
+++ src/datomic_replication/core.clj
@@ -1,39 +1,47 @@
 (ns datomic-replication.core
+  "Datomic Replication"
   (:require [clojure.core.async :as async :refer [go go-loop <! >!]]
             [datomic.api :as d]
-            [enos.core :refer [dochan!]]))
+            [enos.core :refer [dochan!]])
+  (:import [java.util Date]))
 
-
-;;; FIXME - need a marker per source+destination pair
-(def marker-name "/datomic/replication/last-tx")
 
 (defn- init-dest-database
   "This is the default implementation of the `:init` function that you
-  can pass to `replicator`. It just creates the attribute in the
+  can pass to `replicator`. 
+
+  It creates the attribute in the
   destination database that connects each entity to its corresponding
-  entity in the source database."
-  [conn]
+  entity in the source database. It also gets a transaction from the
+  source database as a parameter, so that it can use the
+  txInstant. This is necessary because we need to set the timestamp of
+  this initialization-transaction to be earlier than the first
+  transaction that we will replicate from teh source."
+  [conn tx]
   @(d/transact conn
-               [{:db/id                 (d/tempid :db.part/db)
+               [{:db/id                 (d/tempid :db.part/tx)
+                 ;; tx is the first transaction from the source database. We
+                 :db/txInstant          (Date. (- (.getTime (:db/txInstant tx)) 1000))}
+                {:db/id                 (d/tempid :db.part/db)
                  :db/ident              :datomic-replication/source-eid
                  :db/valueType          :db.type/long
                  :db/unique             :db.unique/identity
                  :db/cardinality        :db.cardinality/one
                  :db.install/_attribute :db.part/db}]))
 
-(defn- e->id-default
-  "This is the default mechanism for getting a database-independent
-  identifier for an entity. It uses a special attribute that gets
-  installed in the destination database by the init function. If your
-  source database has a unique identifier for entities, you can use
-  that instead."
-  [db eid]
-  [:datomic-replication/source-eid eid])
+(def ^:dynamic e->id-default
+  "Function that returns the :ident of an attribute to use as the
+  database-independent identifier for the given entity."
+  (fn [db ent]
+    (if-let [ident (:db/ident ent)]
+      [:db/ident ident]
+      [:datomic-replication/source-eid (:db/id ent)])))
 
-(def default-opts
+(defn default-opts []
   {:init          init-dest-database
    :e->id         e->id-default
    :poll-interval 100})
+
 
 (defn transactions
   "Returns an async channel of transactions.
@@ -44,7 +52,7 @@
   ([conn]
      (transactions conn nil))
   ([conn opts]
-     (let [{:keys [from-t poll-interval]} (merge default-opts opts)
+     (let [{:keys [from-t poll-interval]} (merge (default-opts) opts)
            ch        (async/chan)
            continue? (atom true)
            stopper   #(reset! continue? false)]
@@ -65,139 +73,50 @@
 (defn partition-ident [db eid]
   (:db/ident (d/entity db (d/part eid))))
 
-(defn skip-attr? [ident]
+(defn skip-attr? [attr]
   false)
 
-(defn tx->datoms
-  "Transforms a transaction into a sequence of datoms suitable for
-  transacting to the destination database."
-  [{:keys [t data] :as tx} source-conn e->id]
-  (let [source-db (d/as-of (d/db source-conn) t)]
-    (for [[e a v t added?] data
-          :let [[id-attr id-value] (e->id source-db e)
-                part (partition-ident source-db e)
-                attr-ident (:db/ident (d/entity source-db a))]
-          :when (not (skip-attr? attr-ident))]
-      (if added?
-        (hash-map
-         :db/id     (d/tempid part)
-         id-attr    id-value
-         attr-ident v)
-        [:db/retract
-         [id-attr id-value]
-         attr-ident
-         v]))))
-
-
-
-
-(def ^:dynamic *e->id*
-  "Function that returns the :ident of an attribute to use as the
-  database-independent identifier for the given entity."
-  (fn [db ent]
-    (if-let [ident (:db/ident ent)]
-      [:db/ident ident]
-      [:datomic-replication/source-eid (:db/id ent)])))
-
-(defn mapify-txs
-  "Transforms the :data sequence from a transaction into a sequence of maps.
-   [[e a v t added?] ...] => [{:eid e :added {a-ident v} :retracted {b-ident v}} ...]"
-  [db data]
-  (->> data
-       (reduce (fn [m [e a v t added?]]
-                 (let [attr-ident (:db/ident (d/entity db a))
-                       action     (if added? :added :retracted)]
-                   (assoc-in m [e action attr-ident] v)))
-               nil)
-       (map (fn [[eid m]] (assoc m :eid eid)))))
-
-(defn assoc-partition
-  [db m]
-  (let [part (partition-ident db (:eid m))]
-    (assoc m :part part)))
-
-(defn assoc-identity
-  "Given a map (as produced by mapify-txs), returns it with the ident
-  of the entity assoc'ed in, if it has one."
-  [db m]
-  (if (= :db.part/tx (:part m))
-    (assoc m :ident (d/tempid :db.part/tx))
-    (let [[id-attr id-value] (*e->id* db (d/entity db (:eid m)))]
-      (assoc m :ident [id-attr id-value]))))
-
-(defn ->datoms
-  [{:keys [eid ident added retracted]}]
-  (concat [(when-not (empty? added)
-             (assoc added :db/id ident))]
-          (for [[attr v] retracted]
-            [:db/retract
-             ident
-             attr
-             v])))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-
-
-
-
-;;; Transform the transaction by added some useful crap to it:
-;;;  - mapping from source-eid -> tempid
 
 (defn replicate-tx
   "Sends the transaction to the connection."
   [{:keys [t data] :as tx} source-conn dest-conn e->id]
-  (prn "Got tx:" tx)
+  (prn "Got tx:")
+  (clojure.pprint/pprint tx)
   (let [source-db (d/as-of (d/db source-conn) t)
         dest-db   (d/db dest-conn)
         eids      (doall (distinct (for [[e] data] e)))
-        _  (prn "eids are " eids)
+        _         (prn "eids are " eids)
 
         ;; Create a mapping from the distinct eids in the transaction
         ;; to a tempid in the correct partition.
-        tempids   (into {}
+        e->temp   (into {}
                         (for [eid eids]
                           (let [part (partition-ident source-db eid)]
                             [eid (d/tempid part)])))
 
-        ;; A mapping from eids to idents, for those entities that have idents.
-        idents    (into {}
-                        (for [eid eids
-                              :let [ent (d/entity source-db eid)
-                                    ident (:db/ident ent)]
-                              :when ident]
-                          [eid ident]))
-        
         datoms    (for [[e a v t added?] data
-                        :let [[id-attr id-value] (e->id source-db e)
-                              part (partition-ident source-db e)
-                              attr-ident (:db/ident (d/entity source-db a))]
-                        :when (not (skip-attr? attr-ident))]
+                        :let [ent              (d/entity source-db e)
+                              [id-attr id-val] (e->id source-db ent)
+                              attr             (d/entity source-db a)
+                              attr-ident       (:db/ident attr)
+                              is-ref?          (= :db.type/ref (:db/valueType attr))
+                              v                (if is-ref?
+                                                 (or (e->temp v) v)
+                                                 v)]
+                        :when (not (skip-attr? attr))]
                     (if added?
-                      (if-let [ident (idents e)]
+                      (if (= [:db/ident :db.part/db] [id-attr id-val]) ; FIXME - too specific
                         (hash-map
-                         :db/id     ident
+                         :db/id     id-val
                          attr-ident v)
                         (hash-map
-                         :db/id     (tempids e)
-                         id-attr    id-value
+                         :db/id     (e->temp e)
+                         id-attr    id-val
                          attr-ident v))
                       [:db/retract
-                       [id-attr id-value]
+                       [id-attr id-val]
                        attr-ident
-                       v]))
-
-        ;; _         (->> tx
-        ;;                :data
-        ;;                (mapify-txs source-db)
-        ;;                (map (partial assoc-partition source-db))
-        ;;                (map (partial assoc-identity source-db))
-        ;;                (mapcat ->datoms)
-        ;;                (remove nil?))
-        ]
+                       v]))]
     (prn "transacting:")
     (clojure.pprint/pprint datoms)
     (try
@@ -217,7 +136,7 @@
   ([source-conn dest-conn]
      (replicator source-conn dest-conn nil))
   ([source-conn dest-conn opts]
-     (let [{:keys [init e->id poll-interval] :as opts} (merge default-opts opts)
+     (let [{:keys [init e->id poll-interval] :as opts} (merge (default-opts) opts)
            control-chan  (async/chan)
            [txs stopper] (transactions source-conn opts)
            initialized?  (atom false)]
@@ -228,7 +147,7 @@
                (when (identical? ch txs)
                  (when-not @initialized?
                    (reset! initialized? true)
-                   (init dest-conn))
+                   (init dest-conn (-> tx :data first (nth 3) (->> (d/entity (d/db source-conn))))))
                  (try
                    (replicate-tx tx source-conn dest-conn e->id)
                    (catch Exception e
@@ -238,11 +157,3 @@
          (stop [this]
            (stopper)
            (async/put! control-chan :stop))))))
-
-
-
-
-;;; Experimental - new middleware-based version
-
-;; tx -> group by e? -> determine partitions -> get txInstant -> add tx metadata (source tx, ...)
-;; -> recognize attr installs and add crap -> catch non-existing attrs and retry
